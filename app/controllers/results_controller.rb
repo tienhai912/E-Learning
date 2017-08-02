1fdf187133b9947b0b80fac7189a84a8a542e5fb
mrgnurt <mrgnurt1995@gmail.com>
2017-08-02 14:00:28 +0700

    09 multiple choice

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,45 +1,90 @@
 class ResultsController < ApplicationController
-  attr_reader :test, :supports
+  attr_reader :supports
 
   before_action :authenticate_user!
 
   def new
-    @test = current_user.tests.find_by id: params[:test_id]
-    if test
-      @supports = Supports::ResultSupports.new test: test,
-        word_numerical: params[:word_numerical].to_i
-    else
+    @new_result = Result.new
+    @supports = Supports::ResultSupports.new test_id: params[:test_id],
+      word_numerical: params[:word_numerical].to_i,
+      current_user: current_user
+    unless supports.test
       flash[:danger] = t "find_test_not_found"
       redirect_to root_path
     end
   end
 
   def create
-    result = Result.new result_params
-    check_create_result result
+    word = Word.find_by id: params[:result][:word_id]
+    if word.single_choice?
+      create_result_single
+    else
+      create_result_multiple
+    end
+    reload_page
   end
 
   private
 
-  def check_create_result result
+  def create_result_multiple
+    results = []
+    result_params_multiple[:answer_id].each do |answer_id|
+      result = Result.new test_id: result_params_multiple[:test_id],
+        word_id: result_params_multiple[:word_id],
+        answer_id: answer_id
+      check_create_result result
+      results.push result
+    end
+    show_check_result results
+  end
+
+  def create_result_single
+    result = Result.new result_params_single
     if result.save
       if result.answer.is_correct
         flash[:success] = t "true_answer"
       else
         flash[:danger] = t "false_answer"
       end
-      reload_page
     else
       redirect_to root_path
     end
   end
 
+  def check_create_result result
+    unless result.save
+      flash[:danger] = t "save_result_error"
+      redirect_to root_path
+    end
+  end
+
+  def check_result_multiple results
+    word = Word.find_by id: result_params_multiple[:word_id]
+    return false if results.size != word.answers.correct.length
+    results.each do |result|
+      return false unless result.answer.is_correct
+    end
+    return true
+  end
+
   def reload_page
-    redirect_to new_result_path test_id: result_params[:test_id],
+    redirect_to new_result_path test_id: params[:result][:test_id],
       word_numerical: params[:word_numerical]
   end
 
-  def result_params
+  def result_params_single
     params.require(:result).permit :test_id, :word_id, :answer_id
+  end
+
+  def result_params_multiple
+    params.require(:result).permit :test_id, :word_id, answer_id: []
+  end
+
+  def show_check_result results
+    if check_result_multiple results
+      flash[:success] = t "true_answer"
+    else
+      flash[:danger] = t "false_answer"
+    end
   end
 end
